86ebd5065311368fe6ee6f00c86f4fc663009dde
Kamil <limak18@gmail.com>
2014-05-22 23:51:11 +0200

    modele i migracje

---
+++ jotb/Invoice.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace jotb
+{
+    partial class Invoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogEdi = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialogEdi
+            // 
+            this.openFileDialogEdi.DefaultExt = "edi";
+            this.openFileDialogEdi.FileName = "openFileDialog1";
+            // 
+            // Invoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 401);
+            this.Name = "Invoice";
+            this.Text = "Invoice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogEdi;
+    }
+}
+
---
+++ jotb/Invoice.cs
@@ -0,0 +1,24 @@
+﻿using jotb.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jotb
+{
+    public partial class Invoice : Form
+    {
+
+        public Invoice()
+        {
+            
+        }
+
+    }
+}
